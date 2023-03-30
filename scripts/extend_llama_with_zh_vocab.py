c7d2bf4f4d88f055683a8da09722c1efb58f7489
Ziqing Yang <yangziqing@163.com>
2023-03-30 15:46:38 +0800

    fix typos

--- scripts/extend_llama_with_zh_vocab.py
+++ scripts/extend_llama_with_zh_vocab.py
@@ -13,11 +13,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     model = LlamaForCausalLM.from_pretrained(args.llama_model,torch_dtype="auto")
-    tokenier = LlamaTokenizer.from_pretrained(args.tokenizer)
-    model.resize_token_embeddings(len(tokenier))
+    tokenizer = LlamaTokenizer.from_pretrained(args.tokenizer)
+    model.resize_token_embeddings(len(tokenizer))
 
     model.save_pretrained(args.output_dir)
-    tokenier.save_pretrained(args.output_dir)
+    tokenizer.save_pretrained(args.output_dir)
     
-    assert model.get_input_embeddings().weight.size(0) == len(tokenier)
-    print(f"Extended vocabulary size: {len(tokenier)}")
+    assert model.get_input_embeddings().weight.size(0) == len(tokenizer)
+    print(f"Extended vocabulary size: {len(tokenizer)}")
