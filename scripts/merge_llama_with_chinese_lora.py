219d94b561052927f36529f1ddf8a0d143783236
ymcui <me@ymcui.com>
2023-04-04 20:17:47 +0800

    update print info

--- scripts/merge_llama_with_chinese_lora.py
+++ scripts/merge_llama_with_chinese_lora.py
@@ -168,7 +168,7 @@ def save_shards(lora_model_sd, num_shards: int):
                     new_state_dict[new_k] = v
 
         os.makedirs(output_dir, exist_ok=True)
-        print(f"Saving shard 0 of {num_shards} into {output_dir}/consolidated.00.pth")
+        print(f"Saving shard 1 of {num_shards} into {output_dir}/consolidated.00.pth")
         torch.save(new_state_dict, output_dir + "/consolidated.00.pth")
         with open(output_dir + "/params.json", "w") as f:
             json.dump(params, f)
